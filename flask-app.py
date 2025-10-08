05f3d4b81718cddbb54862919e50751bb91144dd
TanKyeWen <tkyewen@gmail.com>
2025-10-08 15:31:21 +0800

    completed the api call for prediction

--- flask-app.py
+++ flask-app.py
@@ -1,11 +1,47 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
+import numpy as np
+import logging
 
 app = Flask(__name__)
+model = joblib.load("rf_model.pkl")
+feature_order = [
+    "Pregnancy",
+    "Glucose",
+    "BloodPressure",
+    "SkinThickness",
+    "Insulin",
+    "BMI",
+    "DiabetesPeedigreeFunction",
+    "Age"
+]
 
-@app.route('/api/predict', method=('GET'))
+CORS(app)
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+@app.route('/api/predict', methods=['POST'])
 def predict_diabetes():
-    return 0
+    data = request.get_json()
+    height_value = data["Height"]
+    weight_value = data["Weight"]
+
+    BMI = int(weight_value) / pow((int(height_value)/100), 2)
+
+    data["BMI"] = BMI
+
+    for key in list(data.keys()):
+        if key not in feature_order:
+            data.pop(key)
+
+    features = [data[feature] for feature in feature_order]
+    features = np.array(features).reshape(1,-1)
+    prediction = model.predict(features).tolist()
+    logger.info("Predicting with: ", data)
+    logger.info("Outcome: ", prediction)
+    return jsonify({"Result": prediction}), 200
 
 if __name__ == '__main__':
     app.run(
